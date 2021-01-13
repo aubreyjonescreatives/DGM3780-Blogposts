79dcfdd50c11728215c1a5325bcce3b160d2ae73
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-01-12 21:47:09 -0700

    Add scss

--- src/TopNavigation.js
+++ src/TopNavigation.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import BlogpostStyles from './css/BlogpostStyles.css'
 
 
 
 
 function TopNavigation() {
     return (
-      <div className="App">
-      Hello World!
+      <div>
+      <nav className="topNav">
+      <li><a href="#">ABOUT ME</a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      </nav>
       </div>
     );
   }
